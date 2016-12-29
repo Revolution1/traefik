7a406512cb506c44356c21c4e1f2b2a5ddf7e1b3
revol.cai <revol.cai@daocloud.io>
2016-12-29 14:47:14 +0800

    fix typo

--- make-dce-plugin.sh
+++ make-dce-plugin.sh
@@ -4,4 +4,4 @@ set -e
 make generate-webui
 make binary
 
-docker build -f dce-plugin.Dockerfile -t daocloud.io/daocloud/dce-plugin-tarfik .
+docker build -f dce-plugin.Dockerfile -t daocloud.io/daocloud/dce-plugin-traefik .
